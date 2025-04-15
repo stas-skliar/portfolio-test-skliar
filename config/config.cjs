ce2f32bf559af442416f74b40d779be91de4f14e
Stanislav Skliar <stanislav.skliar@bndigital.co>
2025-04-16 02:35:04 +0300

    fix: use DATABASE_URL for Heroku

--- config/config.cjs
+++ config/config.cjs
@@ -1,19 +1,21 @@
 require('dotenv').config();
 const { URL } = require('url');
 
-let config;
+const common = {
+  dialect: 'postgres',
+};
 
 if (process.env.DATABASE_URL) {
   const dbUrl = new URL(process.env.DATABASE_URL);
 
-  config = {
+  module.exports = {
     development: {
+      ...common,
       username: dbUrl.username,
       password: dbUrl.password,
       database: dbUrl.pathname.slice(1),
       host: dbUrl.hostname,
       port: dbUrl.port,
-      dialect: 'postgres',
       dialectOptions: {
         ssl: {
           require: true,
@@ -23,16 +25,14 @@ if (process.env.DATABASE_URL) {
     },
   };
 } else {
-  config = {
+  module.exports = {
     development: {
+      ...common,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      dialect: 'postgres',
     },
   };
 }
-
-module.exports = config;
